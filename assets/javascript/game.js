d3386b19839eca4b29b9c517a971b09ed4015795
Stunna One <stunnawestcoast@icloud.com>
2019-07-21 16:21:17 -0700

    refactored game logic, collector's score is not functioning

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,15 +20,15 @@ var cryValue = {
 
     prism: {
         value: 0
-    },
-}
+    }
+};
 
 // global functions
 
 //random collector score to be reached
 function collectorScoreCreate() {
     collectorScore = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-    $("collectorScore").html(collectorScore);
+    $("#collectorScore").html(collectorScore);
     console.log(collectorScore);
 }
 
@@ -39,7 +39,7 @@ function crystalRandom(min, max) {
 
 // adding values to crystals and to your score
 function addvalues(cryValue) {
-    luminousScore = yourScore + cryValue.value;
+    luminousScore = luminousScore + cryValue.value;
     $("#luminousScore").html(luminousScore);
     winLose();
     console.log("luminous Score: " + luminousScore);
@@ -68,4 +68,56 @@ function restartGame() {
     winCount = 0;
     lossCount = 0;
     luminousScore = 0;
+
+
+    collectorScoreCreate();
+    cryValue.diamond.value = crystalRandom(1, 12);
+    cryValue.cylinder.value = crystalRandom(1, 12);
+    cryValue.oval.value = crystalRandom(1, 12);
+    cryValue.prism.value = crystalRandom(1, 12);
+
+    $("#wins").html(winCount);
+    $("#losses").html(lossCount);
+    $("#luminousScore").html(luminousScore);
+    $("#status").html("Shine on! Click a crystal to begin");
+
+    console.log("diamond: " + cryValue.diamond.value + " | cylinder: " + cryValue.cylinder.value + " | oval: " + cryValue.oval.value + " | prism: " + cryValue.prism.value);
 }
+
+// wins and losses record to check
+function winLose() {
+
+    if (luminousScore === collectorScore) {
+        winCount++;
+        $("#status").text(luminousScore + " Bling! You Won! Click any crystal to begin!")
+        startGame();
+    } else if (luminousScore > collectorScore) {
+        lossCount++;
+        $("#status").text(luminousScore + "oops! To many, Click a any crystal to try again!")
+        startGame();
+    }
+    if (lossCount === 12) {
+        alert("Uh oh, too many rounds lost. Let's try again, Shine on!")
+        restartGame();
+    }
+}
+
+//Crystal Game Main Logic
+$(document).ready(function () {
+
+    restartGame();
+
+    $("#diamond").on("click", function () {
+        addvalues(cryValue.diamond);
+    });
+    $("#cylinder").on("click", function () {
+        addvalues(cryValue.cylinder);
+    });
+    $("#oval").on("click", function () {
+        addvalues(cryValue.oval);
+    });
+    $("#prism").on("click", function () {
+        addvalues(cryValue.prism);
+    });
+
+})
